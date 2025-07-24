d000219ca675c9008092637606130e70cfff03f6
leadond <leadond@gmail.com>
2025-07-24 16:39:25 -0500

    Remove incorrect react-map-gl alias

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      'mapbox-gl': 'mapbox-gl/dist/mapbox-gl.js',
-      'react-map-gl': 'react-map-gl/dist/es/index.js'
+      'mapbox-gl': 'mapbox-gl/dist/mapbox-gl.js'
     }
   },
   optimizeDeps: {
