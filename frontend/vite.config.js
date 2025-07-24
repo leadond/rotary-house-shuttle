1e9ef3aa8afd8e1ca402094210bf9f61bc91005c
leadond <leadond@gmail.com>
2025-07-24 16:28:10 -0500

    Fix mapbox-gl resolution in Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'mapbox-gl': 'mapbox-gl/dist/mapbox-gl.js'
+    }
+  },
   optimizeDeps: {
     include: ['react-map-gl', 'mapbox-gl']
   },
