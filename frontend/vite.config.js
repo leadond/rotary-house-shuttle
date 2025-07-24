a69791c345443c46201baba64914334f349360c5
leadond <leadond@gmail.com>
2025-07-24 16:22:05 -0500

    Fix Vite configuration for react-map-gl

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['react-map-gl', 'mapbox-gl']
+  },
   server: {
     port: 3000
   }
