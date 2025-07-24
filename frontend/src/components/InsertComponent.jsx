4180f9795aa9d89d44c48c273cafc6500df5d1b5
leadond <leadond@gmail.com>
2025-07-24 17:53:27 -0500

    Add default export to InsertComponent

---
+++ frontend/src/components/InsertComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InsertComponent = () => {
+  return (
+    <div>
+      <h3>Insert Component</h3>
+      <p>This is a placeholder component.</p>
+    </div>
+  );
+};
+
+export default InsertComponent;
