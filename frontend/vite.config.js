552f71a37e7d9d2411869e5a7c8081e980fd08ec
leadond <leadond@gmail.com>
2025-07-24 17:04:18 -0500

    Updated vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      'mapbox-gl': 'mapbox-gl/dist/mapbox-gl.js'
-    }
+      'react-map-gl': path.resolve(__dirname, 'node_modules/react-map-gl/dist/esm/index.js'),
+    },
   },
-  optimizeDeps: {
-    include: ['mapbox-gl'],
-  },
-  server: {
-    port: 3000
-  }
-})
+});
