c9d47e2affe7b457d0dd631b16c6d6ffba98d215
leadond <leadond@gmail.com>
2025-07-24 16:51:12 -0500

    Updated main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import 'mapbox-gl/dist/mapbox-gl.css'; // ✅ correct
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import 'mapbox-gl/dist/mapbox-gl.css'; // ✅ CSS for mapbox-gl
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
