a46597da989a6afab79cebe9643394b09c22be46
leadond <leadond@gmail.com>
2025-07-24 17:09:57 -0500

    Exclude react-map-gl from Vite optimization

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['react-map-gl'],
     exclude: ['mapbox-gl']
   }
 });
