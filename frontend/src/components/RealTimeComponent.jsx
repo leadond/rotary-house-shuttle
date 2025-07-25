e521b46173ffd857e34ea1e183d273bd726f6f5c
leadond <leadond@gmail.com>
2025-07-24 22:11:55 -0500

    Add default export to RealTimeComponent

---
+++ frontend/src/components/RealTimeComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RealTimeComponent = () => {
+  return (
+    <div>
+      <h3>Real Time Component</h3>
+    </div>
+  );
+};
+
+export default RealTimeComponent;
